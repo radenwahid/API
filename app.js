0e3aaaf7a943cb6c3c3d60f5d1087335106af88c
radewahid <radenwahiddd29@gmail.com>
2023-12-18 17:43:08 +0700

    main

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-const express = require('express')
+const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const mysql = require("mysql");
+
 const app = express()
 let users = []
 
 app.use(express.json())
 //get,post,delete,put
 
-app.get('/',(req,res)=>{
-    res.send(users)
-})
+app.get("/", async (req, res) => {
+    res.json({ status: "makanan hallo"});
+});
+
+
+app.get('/:makanan', async(req, res) =>{
+    const query = "SELECT * FROM makanan WHERE id = ?";
+    pool.query(query, [ req.params.makanan ], (error, result) => {
+        if (!result[0]){
+            res.json({ status: "Not Found" });
+        }else{
+            res.json(result[0]);
+        }
+    });
+});
+
+const pool =mysql.createPool({
+    user: process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME,
+    socketPath: '/cloudsql/${process.env.INTANCE_CONNECTION_NAME}',
+});
+
+
 app.post('/',(req,res)=>{
    const data = req.body
    users = [...users,data]
